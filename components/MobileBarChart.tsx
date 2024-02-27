4d8cae492226a325d4b2e50b4a3ffc8a8272435b
iampearlep <damilolaprincess4@gmail.com>
2024-02-27 22:52:46 +0100

    feat: update components

---
+++ components/MobileBarChart.tsx
@@ -0,0 +1,84 @@
+"use client"
+import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Bar } from 'react-chartjs-2';
+ 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  
+  export const options = {
+    maintainAspectRatio: true,
+    aspectRatio: 0,
+    responsive: true,
+    scales: {
+        x: {
+            grid: {
+               display: false,
+              },
+              border:{
+                display: false,
+              }
+          },
+          y: {
+            display: true,
+            border:{
+                dash: [6, 4],
+            },
+            beginAtZero: true,
+            ticks: {
+              stepSize: 10000,
+            },
+            grid:{
+                drawTicks: false,
+            }
+          },
+      },
+    plugins: {
+      legend: {
+        position: 'top' as const,
+        display: false,
+      },
+      title: {
+        display: false,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  
+  export const data = {
+    labels,
+    datasets: [
+      {
+        label: '',
+        borderRadius: {topLeft: 15, topRight: 15},
+        data: [6000, 21000, 3000, 27000, 9000, 45000, 9000, 25000, 4000, 32000, 22000, 30000, 50000],
+        backgroundColor: '#d8f5ee',
+        hoverBackgroundColor: '#34caa5',
+      }
+    ],
+  };
+const MobileBarChart = () => {
+  return (
+    <div>
+     <Bar options={options} data={data} />
+    </div>
+  )
+}
+
+export default MobileBarChart
--- components/Trends.tsx
+++ components/Trends.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Chart from '@/public/images/chart.svg'
 import BarChart from './BarChart'
 import { ChevronDownIcon } from '@radix-ui/react-icons'
+import MobileBarChart from './MobileBarChart'
 
 const Trends = () => {
   const [open, setOpen] = React.useState(false)
@@ -12,10 +13,10 @@ const Trends = () => {
     <div className='bg-white w-full  rounded-lg border border-[#e2e4e4] overflow-y-hidden py-4 px-4'>
   <div className='flex flex-col gap-y-10'>
   <div className='flex flex-row justify-between items-center'>
-        <h2 className='font-semibold text-lg'>Sales Trend</h2>
+        <h2 className='font-semibold text-base md:text-lg'>Sales Trend</h2>
        <div className='relative flex flex-row items-center gap-x-2'>
-        <h2 className='font-semibold text-lg'>Sort By:</h2>
-       <button onClick={() => setOpen(!open)} className='flex flex-row px-2 py-2 justify-between items-center rounded-full border border-[#e2e4e4] gap-x-2 text-sm'>Monthly  <ChevronDownIcon /></button>
+        <h2 className='font-semibold text-base md:text-lg'>Sort By:</h2>
+       <button onClick={() => setOpen(!open)} className='flex flex-row px-2 py-2 justify-between items-center rounded-full border border-[#e2e4e4] gap-x-2  text-xs md:text-sm'>Monthly  <ChevronDownIcon /></button>
        {open && 
             <div className='absolute w-full mt-36 ml-6'>
             <div className='bg-white px-1 rounded-md w-9/12 py-2 my-1 shadow-xl mx-auto'>
@@ -29,8 +30,11 @@ const Trends = () => {
         }
        </div>
         </div>
-    <div className='md:h-[336px] flex justify-center items-center'>
+    <div className='md:h-[336px] hidden md:flex justify-center items-center'>
         <BarChart />
+    </div>
+    <div className='flex justify-center items-center md:hidden'>
+      <MobileBarChart />
     </div>
   </div>
     </div>
